b73375d5bf24d60876ab989a080da3c97c1d31c4
Magnus Jason <magnus@magnusjason.com>
2018-04-28 15:28:40 +1000

    Add shitty file listing endpoint

--- HomeModule.cs
+++ HomeModule.cs
@@ -1,4 +1,9 @@
 using Nancy;
+using Nancy.ModelBinding;
+using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
   
 namespace NancyApplication
 {
@@ -8,7 +13,52 @@ namespace NancyApplication
         {
             Get("/", args => "Hello from Nancy running on CoreCLR");
             Get("/test/{name}", args => new Person() { Name = args.name});
+            Post("/listFiles/", args => {
+                ListFilesRequest req = this.Bind<ListFilesRequest>();
+                Console.WriteLine("Received request to list files for " + req.FilePath);
+                string[] directories = Directory.GetDirectories(req.FilePath);
+                List<FileEntry> dirEntries = directories.Select(fileName => {
+                    FileEntry fileEntry = new FileEntry();
+                    DirectoryInfo dirInfo = new DirectoryInfo(fileName);
+                    fileEntry.FileName = dirInfo.Name;
+                    fileEntry.FileSize = 0;
+                    fileEntry.Directory = true;
+                    return fileEntry;
+                    }).ToList();
+
+                string[] files = Directory.GetFiles(req.FilePath);
+
+                List<FileEntry> fileEntries = files.Select(fileName => {
+                    FileEntry fileEntry = new FileEntry();
+                    FileInfo fileInfo = new FileInfo(fileName);
+                    fileEntry.FileName = fileInfo.Name;
+                    fileEntry.FileSize = fileInfo.Length;
+                    fileEntry.Directory = false;
+                    return fileEntry;
+                    }).ToList();
+                
+
+                return dirEntries.Concat(fileEntries);
+            });
         }
+    }
+
+    public class ListFilesRequest 
+    {
+        public string FilePath {get; set;}
+    }
+
+    public class ListFilesResponse 
+    {
+        public FileEntry[] files {get; set;}
+    }
+
+    public class FileEntry 
+    {
+        public string FileName {get; set;}
+        public long FileSize{get; set;}
+
+        public bool Directory{get; set;}
     }
  
     public class Person
