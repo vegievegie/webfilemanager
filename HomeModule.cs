c5182e881905dcda10ad1892d0eba0921d0eb82e
Magnus Jason <magnus@magnusjason.com>
2018-04-28 13:51:02 +1000

    Initial commit

---
+++ HomeModule.cs
@@ -0,0 +1,18 @@
+using Nancy;
+  
+namespace NancyApplication
+{
+    public class HomeModule : NancyModule
+    {
+        public HomeModule()
+        {
+            Get("/", args => "Hello from Nancy running on CoreCLR");
+            Get("/test/{name}", args => new Person() { Name = args.name });
+        }
+    }
+ 
+    public class Person
+    {
+        public string Name { get; set; }
+    }
+}
